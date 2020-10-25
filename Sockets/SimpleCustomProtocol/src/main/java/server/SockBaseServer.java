27f5c61c96d68e3ed2698ceec95f8615ea781202
cekraus1 <cekraus1@asu.edu>
2020-10-25 21:50:08 +0200

    Added SimpleCustomProtocol example

---
+++ Sockets/SimpleCustomProtocol/src/main/java/server/SockBaseServer.java
@@ -0,0 +1,123 @@
+package server;
+
+import java.net.*;
+import java.io.*;
+
+import java.util.Map;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import server.Base;
+
+class SockBaseServer {
+    public static void main (String args[]) throws Exception {
+
+        int count = 0;
+        ServerSocket    serv = null;
+        ObjectInputStream in = null;
+        ObjectOutputStream out = null;
+        Socket clientSocket = null;
+        int port = 9099; // default port
+        int sleepDelay = 10000; // default delay
+        if (args.length != 2) {
+          System.out.println("Expected arguments: <port(int)> <delay(int)>");
+          System.exit(1);
+		}
+        
+        try {
+          port = Integer.parseInt(args[0]);
+          sleepDelay = Integer.parseInt(args[1]);
+        } catch (NumberFormatException nfe) {
+          System.out.println("[Port|sleepDelay] must be an integer");
+          System.exit(2);
+        }
+        try {
+            serv = new ServerSocket(port);
+        } catch(Exception e) {
+          e.printStackTrace();
+          System.exit(2);
+        }
+        while (serv.isBound() && !serv.isClosed()) {
+            System.out.println("Ready...");
+            try {
+                clientSocket = serv.accept();
+                in = new ObjectInputStream(clientSocket.getInputStream());
+                out = new ObjectOutputStream(clientSocket.getOutputStream());
+                String jsonData = (String) in.readObject();
+
+                // convert json string to a JSON object
+                JSONTokener jsonTokener = new JSONTokener(jsonData);
+                JSONObject data = new JSONObject(jsonTokener);
+                
+                // get the 'header' and the 'payload'
+                JSONObject headerJSON = (JSONObject)data.get("header");
+                JSONObject payloadJSON = (JSONObject)data.get("payload");
+
+                Map header = headerJSON.toMap();
+                Map payload = payloadJSON.toMap();
+                
+                Operations operation = getOperation(header);
+                Response response = getResponse(header);
+                int baseN = getBase(header, "base");
+
+                String num1 = getNum(payload, "num1");
+                String num2 = getNum(payload, "num2");
+
+                Base base = new Base();
+                String result = null;
+
+                if (operation == Operations.ADD) {
+                  result = base.add(num1, num2, baseN);
+		  System.out.println("base " + baseN + ": " + num1 + " + " + num2 + " = " + result);
+                } else if (operation == Operations.SUB) {
+                  result = base.substract(num1, num2, baseN);
+		  System.out.println("base " + baseN + ": " + num1 + " - " + num2 + " = " + result);
+                }
+
+                out.writeObject(result);
+            } catch (Exception ex) {
+                ex.printStackTrace();
+            } finally {
+                if (out != null)  out.close();
+                if (in != null)   in.close();
+                if (clientSocket != null) clientSocket.close();
+            }
+        }
+    }
+
+    private static int getBase(Map header, String key) {
+      return Integer.parseInt((String) header.get(key));
+    }
+
+    private static Operations getOperation(Map header) throws RuntimeException {
+      String operation = (String) header.get("operation");
+      if (operation.equals("add")) {
+        return Operations.ADD;
+      } else if (operation.equals("sub")) {
+        return Operations.SUB;
+      } else {
+        throw new java.lang.RuntimeException("Operation not found!");
+      }
+    }
+
+    private static Response getResponse(Map header) throws RuntimeException {
+      String response = (String) header.get("response");
+      if (response.equals("json")) {
+        return Response.JSON;
+      } else {
+        throw new java.lang.RuntimeException("Response type not found!");
+      }
+    }
+
+    private static String getNum(Map payload, String key) {
+      return (String) payload.get(key);
+    }
+}
+
+enum Operations {
+  ADD,
+  SUB
+}
+enum Response {
+  JSON
+}
---
+++ Sockets/WebServer/src/main/java/funHttpServer/GitRepoData.java
@@ -0,0 +1,60 @@
+package funHttpServer;
+
+import org.apache.commons.lang3.builder.ToStringBuilder;
+import org.apache.commons.lang3.builder.ToStringStyle;
+import org.codehaus.jackson.annotate.JsonCreator;
+import org.codehaus.jackson.annotate.JsonProperty;
+import org.codehaus.jackson.annotate.JsonTypeName;
+
+
+public class GitRepoData {
+    private final String name;
+    private final String id;
+    private final Owner owner;
+
+    @JsonCreator
+    public GitRepoData(@JsonProperty("name") String name,
+                       @JsonProperty("id") String id,
+                       @JsonProperty("owner") Owner owner) {
+        this.name = name;
+        this.id = id;
+        this.owner = owner;
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public Owner getOwner() {
+        return owner;
+    }
+
+    @JsonTypeName("owner")
+    public static final class Owner {
+        private final String login;
+
+        @JsonCreator
+        public Owner(@JsonProperty("login") String login) {
+            this.login = login;
+        }
+
+        public String getLogin() {
+            return login;
+        }
+
+
+        @Override
+        public String toString() {
+            return ToStringBuilder.reflectionToString(this, ToStringStyle.JSON_STYLE);
+        }
+    }
+}
