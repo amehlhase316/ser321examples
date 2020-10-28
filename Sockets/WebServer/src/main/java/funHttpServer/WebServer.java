3b2516865c9d44a086fec71cb95044f06ef54933
Jemimah Thomas <jembox@Jemimahs-MacBook-Pro.local>
2020-10-28 00:15:36 -0700

    Trying a new line separator for webpage output

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -272,8 +272,8 @@ class WebServer {
             intOwnerID = objRepoOwner.getInt("id");
 
             System.out.println(strOwnerLogin + ", " + intOwnerID + " -> " + strRepoName);
-            builder.append(strOwnerLogin + ", " + intOwnerID + " -> " + strRepoName);
-            builder.append("\n");
+            builder.append(strOwnerLogin + ", " + intOwnerID + " -> " + strRepoName + System.lineSeparator());
+
 
           } // end for loop
 
