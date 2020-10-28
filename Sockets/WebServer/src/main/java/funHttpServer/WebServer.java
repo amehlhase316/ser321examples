a582de114c3b2f4f2e93afe8ad695885e335ff8f
Jemimah Thomas <jembox@Jemimahs-MacBook-Pro.local>
2020-10-27 23:50:26 -0700

    Testing JSON Parser with console output

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -269,10 +269,13 @@ class WebServer {
             strOwnerLogin = objRepoOwner.getString("login");
             intOwnerID = objRepoOwner.getInt("id");
 
+            String.println(strOwnerLogin + ", " + intOwnerID + " -> " + strRepoName);
+            /*
             builder.append("HTTP/1.1 200 OK\n");
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             builder.append(strOwnerLogin + ", " + intOwnerID + " -> " + strRepoName);
+            */
           } // end for loop
 
         } else {
