3702151f17841511f05f422738a8e88dfacb5774
amehlhase316 <amehlhase316@asu.edu>
2020-11-05 13:44:51 -0700

    Add started code for Assign 4 task 1

---
+++ Assignments/Assignment 4/Task 1/StringList.java
@@ -0,0 +1,26 @@
+import java.util.List;
+import java.util.ArrayList;
+
+class StringList {
+    
+    List<String> strings = new ArrayList<String>();
+
+    public void add(String str) {
+        int pos = strings.indexOf(str);
+        if (pos < 0) {
+            strings.add(str);
+        }
+    }
+
+    public boolean contains(String str) {
+        return strings.indexOf(str) >= 0;
+    }
+
+    public int size() {
+        return strings.size();
+    }
+
+    public String toString() {
+        return strings.toString();
+    }
+}
