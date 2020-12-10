0f2adc8e15578bc618887af5ba69c07563f7760c
Kevin Moore <kevinmoore@localhost.localdomain>
2020-12-10 06:11:36 -0500

    Update WebServer multiply error checking

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -199,10 +199,11 @@ class WebServer {
 
           Map<String, String> query_pairs = new LinkedHashMap<String, String>();
           // extract path parameters
-          query_pairs = splitQuery(request.replace("multiply?", ""));
-
-          // extract required fields from parameters
           try{
+            query_pairs = splitQuery(request.replace("multiply?", ""));
+
+            // extract required fields from parameters
+          
             Integer num1 = Integer.parseInt(query_pairs.get("num1"));
             Integer num2 = Integer.parseInt(query_pairs.get("num2"));
 
@@ -221,6 +222,11 @@ class WebServer {
             builder.append("Content-Type: text/html; charset=utf-8\n");
             builder.append("\n");
             builder.append("Both inputs must be integers");
+          }catch (StringIndexOutOfBoundsException e){
+            builder.append("HTTP/1.1 406 Not Acceptable\n");
+            builder.append("Content-Type: text/html; charset=utf-8\n");
+            builder.append("\n");
+            builder.append("There must be two inputs to process");
           }
           
           // TODO: Include error handling here with a correct error code and
