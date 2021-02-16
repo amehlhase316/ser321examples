be12600862f0ff320b247a5446048a6915f429b5
amehlhase316 <amehlhase316@asu.edu>
2021-02-15 20:23:09 -0700

    Fixes on JavaSimpleSock and EchoServer

--- Sockets/JavaSimpleSock/README.md
+++ Sockets/JavaSimpleSock/README.md
@@ -1,8 +1,6 @@
 ##### Author: Instructor team SE, ASU Polytechnic, CIDSE, SE
-* Version: February 2021
+* Version: Sept 2020
 
 ##### Purpose
 Each program has a short description as well as the Gradle file
 * Please run `SockServer` and `SockClient` together.
-
-SockServer initiates a simple socket server that awaits a String and Integer object from the client. The server will loop infinitely until the exit command is received, at which point it will drop the current client connection and await a new connection.
--- Sockets/JavaSimpleSock/src/main/java/SockServer.java
+++ Sockets/JavaSimpleSock/src/main/java/SockServer.java
@@ -7,72 +7,43 @@ import java.io.*;
  * see http://pooh.poly.asu.edu/Ser321
  * @author Tim Lindquist Tim.Lindquist@asu.edu
  *         Software Engineering, CIDSE, IAFSE, ASU Poly
- * @version February 2021
+ * @version August 2020
  * 
  * @modified-by David Clements <dacleme1@asu.edu> September 2020
- * @modified-by Kevin Moore <klmoor21@asu.edu> February 2021
  */
 public class SockServer {
   public static void main (String args[]) {
-    Socket sock = null;
-    Boolean connected = false;
-    ObjectInputStream in = null;
-    OutputStream out = null;
-    ObjectOutputStream os = null;
-    String receviedString = "";
-    Integer receivedInt = 0;
+    Socket sock;
     try {
-        //open socket
-        ServerSocket serv = new ServerSocket(8888); // create server socket on port 8888
-        System.out.println("Server ready for a connection");
+      //open socket
+      ServerSocket serv = new ServerSocket(8888); // create server socket on port 8888
+      System.out.println("Server ready for 3 connections");
+      // only does three connections then closes
+      // NOTE: SINGLE-THREADED, only one connection at a time
+      for (int rep = 0; rep < 3; rep++){
+        System.out.println("Server waiting for a connection");
+        sock = serv.accept(); // blocking wait
+        // setup the object reading channel
+        ObjectInputStream in = new ObjectInputStream(sock.getInputStream());
         
-        //loop infinitely to get a connection and exchange messages
-        while(true){
-          
-            //if not connected try to connect
-            if(!connected){
-                System.out.println("Server waiting for a connection");
-                sock = serv.accept(); // blocking wait
-                // setup the object reading and writing channels
-                in = new ObjectInputStream(sock.getInputStream());
-                out = sock.getOutputStream();
-                os = new ObjectOutputStream(out);
-                connected = true;
-            }
+        // read in one object, the message. we know a string was written only by knowing what the client sent. 
+        // must cast the object from Object to desired type to be useful
+        String s = (String) in.readObject();
+        System.out.println("Received the String "+s);
+        // read in the number, we know it's an integer because that's the second thing sent by the client.
+        Integer i = (Integer) in.readObject();
+        System.out.println("Received the Integer "+ i);
 
-            //if input stream is initialized, read in object
-            if (in != null){
-                // read in one object, the message. we know a string was written only by knowing what the client sent. 
-                // must cast the object from Object to desired type to be useful
-                receviedString = (String) in.readObject();
-                System.out.println("Received the String "+receviedString);
-                // read in the number, we know it's an integer because that's the second thing sent by the client.
-                receivedInt = (Integer) in.readObject();
-                System.out.println("Received the Integer "+ receivedInt);
-
-            }
-
-            //if ouput stream is initialized write output
-            if (out != null && os != null){
-
-                //if the string received is exit or integer received is 0 exit
-                if(receviedString.equalsIgnoreCase("exit") || receivedInt == 0){
-                
-                  os.writeObject("exiting");
-                  //close the streams and socket server
-                os.close();
-                in.close();
-                sock.close();
-                connected = false;
-                
-                }else{
-                  os.writeObject(receivedInt + " and " + receviedString + " ... Got it!");
-                }
-            }
-        }  
-      } catch(Exception e) {
-        e.printStackTrace();
-        
+        // generate an output
+        // get output channel
+        OutputStream out = sock.getOutputStream();
+        // create an object output writer (Java only)
+        ObjectOutputStream os = new ObjectOutputStream(out);
+        // write the whole message
+        os.writeObject("Got it!");
+        // make sure it wrote and doesn't get cached in a buffer
+        os.flush();
       }
+    } catch(Exception e) {e.printStackTrace();}
   }
 }
