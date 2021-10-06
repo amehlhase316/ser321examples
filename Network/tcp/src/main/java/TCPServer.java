a2ce6cec35adc69796dc238bad9858639e975e49
unknown <Gjednesty@gmail.com>
2021-10-05 22:25:56 -0400

    Add comments to Network examples

--- Network/tcp/src/main/java/TCPServer.java
+++ Network/tcp/src/main/java/TCPServer.java
@@ -1,6 +1,8 @@
 import java.net.*;
 import java.io.*;
-
+/**
+ * A server class for client-server connections using a TCP connection.
+ */
 public class TCPServer {
 	public static void main (String args[]) {
 		ServerSocket listenSocket = null;
@@ -9,7 +11,7 @@ public class TCPServer {
           System.exit(0);
         }
         int portNo = 9099; // default port
-        int delay = 9; // default port
+        int delay = 9; // default delay
         try {
             portNo = Integer.parseInt(args[0]);
             delay = Integer.parseInt(args[1]);
@@ -18,8 +20,8 @@ public class TCPServer {
             System.exit(2);
         }
 		try {
-			listenSocket = new ServerSocket(portNo);
-			while(true) {
+			listenSocket = new ServerSocket(portNo); // initialize a server socket listening for connections from portNo
+			while(true) { // Loop for server socket to accept new connections as they are received
 				Socket clientSocket = listenSocket.accept();
 				new Connection(clientSocket, delay);
 			}
@@ -37,9 +39,14 @@ public class TCPServer {
 	}
 }
 
+/**
+ * Connection class for TCP server that extends Thread class
+ * this is used to create a thread for a client connection and create a DataInputStream and DataOutputStream
+ * to handle communication between the two.
+ */
 class Connection extends Thread {
-	DataInputStream in;
-	DataOutputStream out;
+	DataInputStream in; // data input stream lets the application read primitive Java data types.
+	DataOutputStream out; //data output stream lets the application write data that can be read by a data input stream.
 	Socket clientSocket;
 	long __msDelay;
 
@@ -47,19 +54,23 @@ class Connection extends Thread {
 		try {
 			clientSocket = aClientSocket;
 			in = new DataInputStream( clientSocket.getInputStream());
-			out =new DataOutputStream( clientSocket.getOutputStream());
+			out = new DataOutputStream( clientSocket.getOutputStream());
 			__msDelay = msDelay;
 			this.start();
 		} catch(IOException e) {
 			System.out.println("Connection:"+e.getMessage());
 		}
 	}
+
+	/**
+	 * Run method for Connection thread.
+	 */
 	public void run(){
 		try {			                 // an echo server
 			String data = in.readUTF();	 // read a line of data from the stream
 			System.out.println("Read " + data);
-			Thread.sleep(__msDelay);
-			out.writeUTF(data);
+			Thread.sleep(__msDelay); // suspends current thread for specified delay
+			out.writeUTF(data); // writes data to output stream using modified UTF-8 encoding
 		} catch (EOFException e) {
 			System.out.println("EOF:"+e.getMessage());
 		} catch(IOException e) {
@@ -69,7 +80,7 @@ class Connection extends Thread {
 		}
 		finally { 
 			try {
-				clientSocket.close();
+				clientSocket.close(); // closes client socket
 			} catch (IOException e) {
 				/*close failed*/
 			}
