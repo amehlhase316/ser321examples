8f3098e94a0f06ecf32eb57bbab432a1ff7987bd
amehlhase316 <amehlhase316@asu.edu>
2022-03-02 17:05:50 -0700

    Add SynchExample3 to Synchronization to show that one can get inconsistent reads

--- Threads/Synchronization/src/main/java/SynchExample.java
+++ Threads/Synchronization/src/main/java/SynchExample.java
@@ -1,9 +1,9 @@
 
 class Account {
-	int     numTransactions = 0;
-	int     balance = 0;
+	int numTransactions = 0;
+	int balance = 0;
 
-	public synchronized  void deposit(int amount) {
+	public synchronized void deposit(int amount) {
 		balance += amount;
 		// inc trans
 		try {
@@ -12,10 +12,10 @@ class Account {
 			t.printStackTrace();
 		}
 		numTransactions++;
-		System.out.println("Balance (deposit): " + balance); // prints the balanace after the transactions
+		System.out.println("Balance (deposit): " + balance + ", num: " + numTransactions); // prints the balanace after the transactions
 	}
 
-	public synchronized  void withdraw(int amount) {
+	public synchronized void withdraw(int amount) {
 		balance -= amount;
 		// inc trans
 		try {
@@ -32,6 +32,11 @@ class Account {
 	}
 	public synchronized int getNumberOfTransactions() {
 		return numTransactions;
+	}
+
+	// method that gets both balance and numTransactions since getting them separately might still seem like an inconsistent state
+	public synchronized String getBoth(){ 
+		return ("Synch:"+ balance + " " + numTransactions);
 	}
 }
 
@@ -76,38 +81,34 @@ class SynchExample extends Thread {
 			trans.start(); // start new thread
 		}
 		// All trans done
-		Thread.sleep(sleepDelay); // if sleep is too short the balance will be printed before threads are done
+		Thread.sleep(sleepDelay);                                                                // if sleep is too short the balance will be printed before threads are done
 		System.out.println("Balance is " + account.getBalance() + " from " + account.getNumberOfTransactions() + " transactions");
 	}
 }
 
-
-// similar example but with two accounts. Whatever goes into one account goes into the other
-class SynchExample2 extends Thread {
-	Account account1;
-	Account account2;
+// Example which also prints the inconsistent states when just accessing the account attributes
+// showing the consistent state with getBoth()
+class SynchExample3 extends Thread {
+	Account account;
 	int id;
 
-	public SynchExample2(int id, Account account1, Account account2) {
+	public SynchExample3(int id, Account account) {
 		super("Transaction #" + id);
-		this.account1 = account1;
-		this.account2 = account2;
+		this.account = account;
 		this.id = id;
 	}
 
 	public void run() {
 		System.out.println("Transaction started #" + id);
 		for (int i = 1; i <= 3; i++) {
-			account2.withdraw(id*i);
-			account1.deposit(id*i);
+			account.deposit(id*i);
 			// deposit done
-			System.out.println("Back from deposit/withdraw in thread " + i);
+			System.out.println("Back from deposit in thread " + id);
 		}
 	}
 
 	public static void main(String args[]) throws Exception {
-		Account account1 = new Account();
-		Account account2 = new Account();
+		Account account = new Account();
 
         if (args.length != 2) {
                 System.out.println("Expected Arguments: <transactions(int)> <sleep(int)>");
@@ -122,14 +123,36 @@ class SynchExample2 extends Thread {
               System.out.println("[transactions|sleep] must be integer");
               System.exit(2);
           }
-
         // number of transactions
-		for (int i=1; i <= transactions; i++) {
-			SynchExample2 trans = new SynchExample2(i, account1, account2); // create new thread
-			trans.start(); //start new thread
+		for (int i=1; i <= transactions; i++) { 
+			SynchExample trans = new SynchExample(i, account); // create new thread
+			trans.start(); // start new thread
 		}
+
+		// since balance and numTransactions is public we can print these in between and see inconsistent states. Better to have them private and thus only reach them through get/set which are synchronized
+		System.out.println(account.balance + " " + account.numTransactions);
+		System.out.println(account.getBoth());
+		Thread.sleep(sleepDelay);    
+		System.out.println(account.balance + " " + account.numTransactions);
+		System.out.println(account.getBoth());
+		Thread.sleep(sleepDelay);    
+		System.out.println(account.balance + " " + account.numTransactions);
+		System.out.println(account.getBoth());
+		Thread.sleep(sleepDelay);    
+		System.out.println(account.balance + " " + account.numTransactions);
+		System.out.println(account.getBoth());
+		Thread.sleep(sleepDelay);    
+		System.out.println(account.balance + " " + account.numTransactions);
+		System.out.println(account.getBoth());
+		Thread.sleep(sleepDelay);    
+		System.out.println(account.balance + " " + account.numTransactions);
+		System.out.println(account.getBoth());
+		Thread.sleep(sleepDelay);    
+		System.out.println(account.balance + " " + account.numTransactions);
+		System.out.println(account.getBoth());
+		
 		// All trans done
-		Thread.sleep(sleepDelay);
-		System.out.println("Balance is " + account1.getBalance() + " from " + account1.getNumberOfTransactions() + " transactions");
+		Thread.sleep(sleepDelay);                                                                // if sleep is too short the balance will be printed before threads are done
+		System.out.println("Balance is " + account.getBalance() + " from " + account.getNumberOfTransactions() + " transactions");
 	}
 }
