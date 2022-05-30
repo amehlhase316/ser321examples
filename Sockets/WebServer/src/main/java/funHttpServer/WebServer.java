2dfd0f9781585604b566e918e58189893cc2d954
bperry13 <bperry13@asu.edu>
2022-05-30 13:09:10 -0500

    Add error handling to multiply

--- Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
+++ Sockets/WebServer/src/main/java/funHttpServer/WebServer.java
@@ -197,22 +197,35 @@ class WebServer {
           // This multiplies two numbers, there is NO error handling, so when
           // wrong data is given this just crashes
 
-          Map<String, String> query_pairs = new LinkedHashMap<String, String>();
-          // extract path parameters
-          query_pairs = splitQuery(request.replace("multiply?", ""));
+          try {
+            Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+            // extract path parameters
+            query_pairs = splitQuery(request.replace("multiply?", ""));
+            if (query_pairs.containsKey("num1") && query_pairs.containsKey("num2")) {
+              if (!query_pairs.isEmpty()) {
 
-          // extract required fields from parameters
-          Integer num1 = Integer.parseInt(query_pairs.get("num1"));
-          Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+                // extract required fields from parameters
+                Integer num1 = Integer.parseInt(query_pairs.get("num1"));
+                Integer num2 = Integer.parseInt(query_pairs.get("num2"));
+       
+                // do math
+                Integer result = num1 * num2;
 
-          // do math
-          Integer result = num1 * num2;
+                // Generate response
+                builder.append("HTTP/1.1 200 OK\n");
+                builder.append("Content-Type: text/html; charset=utf-8\n");
+                builder.append("\n");
+                builder.append("Result is: " + result);
+              }
+            } else {
+              Integer result = 0;
+              System.out.println("Error.Try the format http://54.236.237.195:9000/multiply?num1=3&num2=4");
+            }
+          } catch (StringIndexOutOfBoundsException e) {
+            Integer result = 0;
+            System.out.println("Error. Connection closed.");
+          }
 
-          // Generate response
-          builder.append("HTTP/1.1 200 OK\n");
-          builder.append("Content-Type: text/html; charset=utf-8\n");
-          builder.append("\n");
-          builder.append("Result is: " + result);
 
           // TODO: Include error handling here with a correct error code and
           // a response that makes sense
