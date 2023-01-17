8de9735843aa04912fe611959413ddbde849cec1
ajcarso2 <ajcarso2@asu.edu>
2023-01-16 22:12:42 -0700

    Changed Local host to actual address

--- Sockets/JavaSimpleSock2/src/main/java/SockClient.java
+++ Sockets/JavaSimpleSock2/src/main/java/SockClient.java
@@ -14,7 +14,7 @@ import java.io.*;
 class SockClient {
   public static void main (String args[]) {
     Socket sock = null;
-    String host = "localhost";
+    String host = "ec2-35-165-121-165.us-west-2.compute.amazonaws.com";
     String message = "HI";
     Integer number = 100;
 
@@ -33,7 +33,7 @@ class SockClient {
     
     try {
       // open the connection
-      sock = new Socket(host, 8888); // connect to host and socket on port 8888
+      sock = new Socket(host, 8080); // connect to host and socket on port 8888
       // get output channel
       OutputStream out = sock.getOutputStream();
       // create an object output writer (Java only)
